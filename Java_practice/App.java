ea39557ec3fe110df8bab12ea5e482e35eb6e219
geo-theo <theojsymonds@gmail.com>
2026-01-27 21:50:48 -0700

    Create App.java

---
+++ Java_practice/App.java
@@ -0,0 +1,19 @@
+/*
+* author: Theo Symonds
+* date: Fri, Jan 23
+* title: TJS-CSCI152-Lab01
+*/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("*******************************************");
+
+        System.out.println("*   Name: Theo Symonds    *");
+
+        System.out.println("*   Track: Geography    *");
+        
+        System.out.println("*   Favorite Color: Blue    *");
+
+        System.out.println("*******************************************");
+    }
+}
